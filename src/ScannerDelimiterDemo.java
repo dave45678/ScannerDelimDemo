93ef08ffa4e17261882a9654b37d1f6f1308f68c
Dave Wolf <dave45678@gmail.com>
2016-09-16 14:26:12 -0400

    initial commit

---
+++ src/ScannerDelimiterDemo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ScannerDelimiterDemo {
+
+	public static void main(String[] args) {
+
+		// Initialize Scanner object
+		Scanner scan = new Scanner("Anna Mills/Female/18");
+		// initialize the string delimiter
+		scan.useDelimiter("/");
+		// Printing the delimiter used
+		System.out.println("The delimiter use is "+scan.delimiter());
+		// Printing the tokenized Strings
+		while(scan.hasNext()){
+			System.out.println(scan.next());
+		}
+		// closing the scanner stream
+		scan.close();
+
+	}
+
+}
